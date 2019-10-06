e4ab368eb4fcd4081b48e25bf96e75f3084763c9
Alexis Kondilis <kondilisalexis@gmail.com>
2019-10-06 22:14:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,19 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+
+    elsif req.path.match(/cart/)
+      if @@cart.each do |item|
+        resp.write "#{item}\n"
+      else
+        @@cart.inlcude?
+        resp.write "Your cart is empty"
+      end
+      
     else
       resp.write "Path Not Found"
     end
